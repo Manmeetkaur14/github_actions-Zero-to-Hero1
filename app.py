5d618840c8ae02a934ca44931ef578e6534a6815
Manmeetkaur14 <sohaildolly225@gmail.com>
2026-03-30 11:29:17 +0530

    sfg

--- app.py
+++ app.py
@@ -7,8 +7,10 @@ def hello_world():
     return render_template('index.html')
 
 
+
 @app.route('/health')
 def health():
     return 'Server is up and running'
+
 
 app.run(debug=True, host='0.0.0.0', port=80)
